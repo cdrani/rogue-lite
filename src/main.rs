e3ae1b57ff6793dc8f082d7fa803fbc74f1570ab
cdrani <charlesdrani@gmail.com>
2024-03-06 10:13:17 -0700

    feat: render Player on Map

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,24 @@ use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        self.map.render(ctx)
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
