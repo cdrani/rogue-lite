da2a4fe0d7e9e5823413cb9ac06b484caa489393
cdrani <charlesdrani@gmail.com>
2024-03-06 09:03:24 -0700

    init commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
+
+fn main() {
+    println!("Hello, world!");
+}
