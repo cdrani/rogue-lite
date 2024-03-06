a40fe621ef211baa5585692a83af97d062c27343
cdrani <charlesdrani@gmail.com>
2024-03-06 09:18:31 -0700

    feat: create empty Map

---
+++ src/map.rs
@@ -0,0 +1,23 @@
+use std::usize;
+
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+}
