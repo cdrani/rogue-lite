a33d8c7871e653adeb4487b96ecb2357001a9a14
cdrani <charlesdrani@gmail.com>
2024-03-07 09:43:28 -0700

    feat: create Scheduler & system executor

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod components;
 mod map;
 mod map_builder;
 mod spawner;
+mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -20,6 +21,7 @@ mod prelude {
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::spawner::*;
+    pub use crate::systems::*;
 }
 
 use prelude::*;
@@ -57,7 +59,9 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
 
-        // TODO: Execute Systems
+        self.resources.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+
         // TODO: Render Draw Buffer
     }
 }
