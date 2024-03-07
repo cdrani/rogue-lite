586bf155151c6249b4c505308b3d666ec67ea78a
cdrani <charlesdrani@gmail.com>
2024-03-06 17:27:25 -0700

    feat: render graphics in layers

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
+
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -40,9 +44,15 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
         .with_title("RogueLT")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_HEIGHT, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
