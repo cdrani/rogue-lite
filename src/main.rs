db1c1786d6981a9e5463501356947ced7d4a5c43
cdrani <charlesdrani@gmail.com>
2024-03-07 08:43:10 -0700

    docs: add legion crate

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
 
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
